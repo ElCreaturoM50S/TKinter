3b48dcc472ec5b018f758ad4f57b2c26c1c6e614
ElCreaturoM50S <elcreaturo@M50S.pl>
2022-09-29 23:40:35 +0200

    Dodanie sus2 i button

--- TKinter.py
+++ TKinter.py
@@ -1,3 +1,4 @@
+from distutils.command.config import config
 from tkinter import Tk,Label,Button, Entry
 
 window = Tk()
@@ -7,5 +8,11 @@ window.minsize(420,420)
 lb1=Label(text="sus 1")
 lb1.grid(column=0, row=1)
 
+lb2=Label(text="sus 2")
+lb2.grid(column=0, row=2)
+
+btn = Button()
+btn.config(text="przycisk")
+btn.grid(column=0, row=3)
 
 window.mainloop()
